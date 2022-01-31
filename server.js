b3a704c1274d0a4b1ce7b9d44865f898111690e9
meawi <westerlund.ww@gmail.com>
2022-01-31 09:58:41 +0200

    register

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-const express = require("express");
+const express = require("express")
+const server = express()
+const mongoose = require("mongoose")
+server.use(express.static("public"))
+server.use(express.urlencoded({extended:true}))
 
-const PORT = 3000;
-const app = express();
+mongoose.connect("mongodb+srv://meawi:anjing123@cluster0.cos6p.mongodb.net/team?retryWrites=true&w=majority")
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public" + "/index.html")
-});
+const team = mongoose.connection
 
-app.listen(PORT, () => {
- console.log(`Server is listening on port: ${PORT}`);
-});
+team.on('open', (err)=>{
+    if(err)throw err
+    console.log('ansluten till databas');
+})
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const Users = mongoose.model('usersModel', userSchema, 'users')
+
+server.post("/register", (req, res)=>{
+    let name = req.body.name
+    let email = req.body.email
+    let password = req.body.password
+    let data = new Users({
+        name: name,
+        password: password,
+        email: email
+    })
+    data.save((err)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log("sparade användare till databasen");
+    })
+    res.redirect("/")
+})
+
+server.listen(3469)
